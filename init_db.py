7916e7454786b5beef24a212b5e91bd8e64f26a6
birabalaz <eriick-monteiro>
2025-10-25 00:26:37 -0300

    :recycle: refactor: Upgrading database creation

--- init_db.py
+++ init_db.py
@@ -1,39 +1,44 @@
-from dotenv import load_dotenv
 import sqlite3
+from pathlib import Path
 import os
 
+ROOT_FILE = Path(__file__).parent
+DB_NAME = 'database.db'
+DB_FOLDER = 'database'
+DB_FILE = ROOT_FILE / DB_FOLDER / DB_NAME
 
-# Loading Environment Variables
-load_dotenv()
-
-DB_NAME = os.getenv("DB_NAME")
-TABLE_USERS = os.getenv("TABLE_USERS")
-TABLE_POSTS = os.getenv("TABLE_POSTS")
-
+# Creating database directory if not exists
+if not os.path.exists(DB_FOLDER):
+    os.mkdir(DB_FOLDER)
+    print(f"✅ Folder {DB_FOLDER} created!")
+else:
+    print(f"➖ Folder {DB_FOLDER} already exists")
 
 # Creating Connection and Cursor
-try:
-    conn = sqlite3.connect(f'database/{DB_NAME}.db')
-    cursor = conn.cursor()
+if not os.path.exists(DB_FILE):
+    try:
+        conn = sqlite3.connect(f'database/{DB_NAME}')
+        cursor = conn.cursor()
 
-    print(f"✅ Database '{DB_NAME}' successfuly created.")
-except:
-    print(f"❌ Error. Something went wrong on creating of database {DB_NAME}.")
+        print(f"✅ Database '{DB_NAME}' successfuly created.")
 
+        # Creating Users Table in Database
+        try:
+            cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL
+            )
+            ''')
 
-# Creating Users Table in Database
-try:
-    cursor.execute(f'''
-    CREATE TABLE IF NOT EXISTS {TABLE_USERS} (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password_hash TEXT NOT NULL
-    )
-    ''')
+            conn.commit()
+            conn.close()
 
-    conn.commit()
-    conn.close()
-
-    print(f"✅ Table '{TABLE_USERS}' successfuly created.")
-except:
-    print(f"❌ Error. Something went wrong on creating of {TABLE_USERS} table.")
+            print(f"✅ Table 'users' successfuly created.")
+        except:
+            print(f"❌ Error. Something went wrong on creating of 'users' table.")
+    except:
+        print(f"❌ Error. Something went wrong on creating of {DB_NAME}.")
+else:
+    print(f"➖ {DB_NAME} already exists")
